f914f61cb08dfec1bad0c533d78b7a5b986ebb2f
Florian Thiel <flth@flth.de>
2023-07-09 14:21:33 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,36 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "DB Facs 124 Bogie bulk goods wagon",
+		["mod_desc"] = "The wagons, originally designated Eads-y and procured by the Deutsche Reichsbahn from 1978 onwards, are intended for the transport of bulk goods that are insensitive to moisture. The loading space has the shape of several funnels standing next to each other. The gravity of the load is used for the unloading process. The discharge openings with round slides allow the wagon to be unloaded completely to one side or the other. The swivelling discharge chutes allow bunker unloading.",
+
+        ["Facs124_menu"] = "Facs 124",
+		["Eads-y_dr_name"] = "Eads-y DR (1978)",
+		["Eads-y_dr_desc"] = "The first wagons were built in France on behalf of the Deutsche Reichsbahn from 1978. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
+        ["Facs124_dr_name"] = "Facs DR (1980)",
+        ["Facs124_dr_desc"] = "From 1980 onwards, the wagons originally designated as Eads-y were listed as Facs according to UIC guidelines. Their use remained the same.",
+        ["Facs124_db_name"] = "Facs 124 DB (1991)",
+        ["Facs124_db_desc"] = "After the reunification, the Facs 124 also came to the Deutsche Bundesbahn.",
+        ["Facs124_dbag_name"] = "Facs 124 DB AG (1994)",
+        ["Facs124_dbag_desc"] = "After the founding of DB AG, the Facs 124 continued to be used with the new logo.",
+        ["Facs124_vr_name"] = "Facs 124 verkehrsrot DB AG (2007)",
+        ["Facs124_vr_desc"] = "Later on, the cars were also repainted in traffic red. So far, some of them are still in use.",
+	},
+	de = {
+		["mod_name"] = "Facs 124 Drehgestell-Schüttgutwagen",
+		["mod_desc"] = "Die ursprünglich als Eads-y bezeichneten, von der Deutschen Reichsbahn ab 1978 beschafften Wagen sind zur Beförderung von Nässe unempfindlichem Schüttgut vorgesehen. Der Laderaum hat die Form mehrerer nebeneinander stehender Trichter. Für den Entladevorgang wird die Schwerkraft des Ladegutes genutzt. Die Auslauföffnungen mit Rundschiebern ermöglichen die restlose Entladung des Wagens nach der einen oder der anderen Seite. Die schwenkbaren Auslaufrutschen gestatten eine Bunkerentladung.",
+
+        ["Facs124_menu"] = "Facs 124",
+		["Eads-y_dr_name"] = "Eads-y DR (1978)",
+		["Eads-y_dr_desc"] = "Die ersten Wagen wurden im Auftrag der Deutschen Reichsbahn ab 1978 in Frankreich gebaut. Die Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
+        ["Facs124_dr_name"] = "Facs DR (1980)",
+        ["Facs124_dr_desc"] = "Ab dem Jahr 1980 wurden die ursprünglich als Eads-y bezeichneten Wagen nach UIC-Richtline als Facs geführt. Ihre Verwendung blieb gleich.",
+        ["Facs124_db_name"] = "Facs 124 DB (1991)",
+        ["Facs124_db_desc"] = "Nach der Wende kamen die Facs 124 auch zur Deutschen Bundesbahn.",
+        ["Facs124_dbag_name"] = "Facs 124 DB AG (1994)",
+        ["Facs124_dbag_desc"] = "Nach Gründung der DB AG wurden die Facs 124 weiterverwendet mit dem neuen Logo.",
+        ["Facs124_vr_name"] = "Facs 124 verkehrsrot DB AG (2007)",
+        ["Facs124_vr_desc"] = "Im späteren Verlauf wurden die Wagen auch mal in verkehrsrot umlackiert. Bisher sind immer noch einige im Einsatz.",
+	},
+}
+end
