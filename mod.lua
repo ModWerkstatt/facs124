52691476127b4fef450d61a878b7ab48abee0bd1
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 14:49:55 +0200

    Update auf 1.2
    Fake hinzugefügt

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 1,
+		minorVersion = 2,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
@@ -13,16 +13,50 @@ return {
 		    },
 		},
 		tags = { "europe", "waggon", "schuettgut", "deutschland", "germany", "UIC", "gueterwagen", "db" },
-		tfnetId = { },
 		minGameVersion = 0,
 		dependencies = { },
 		url = { "" },
+
+        params = {
+			{
+				key = "facs124_fake",
+				name = _("Fake_facs124_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_facs124_desc"),
+				defaultIndex = 0,
+			},	
+        },
 	},
 	options = {
 	},
 	
-	runFn = function (settings)
-	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["y_dr_fake.mdl"] = true, 
+			["db_fake.mdl"] = true, 
+			["dbag_fake.mdl"] = true, 
+			["dr_fake.mdl"] = true, 
+			["vr_fake.mdl"] = true, 
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/Eads-([^/]*.mdl)')
+							or fileName:match('/Facs124_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["facs124_fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+		addModifier( "loadModel", metadataHandler )
 	end
 	}
 end
--- strings.lua
+++ strings.lua
@@ -4,6 +4,12 @@ return {
 		["mod_name"] = "DB Facs 124 Bogie bulk goods wagon",
 		["mod_desc"] = "The wagons, originally designated Eads-y and procured by the Deutsche Reichsbahn from 1978 onwards, are intended for the transport of bulk goods that are insensitive to moisture. The loading space has the shape of several funnels standing next to each other. The gravity of the load is used for the unloading process. The discharge openings with round slides allow the wagon to be unloaded completely to one side or the other. The swivelling discharge chutes allow bunker unloading.",
 
+        ["Fake_tamns_wagen"] = "Enable Facs 124 Fake Versions?",
+        ["option_fake_tamns_desc"] = "Should the fake versions of the Facs 124 be available in the game?",
+		
+		["sound_check"] = "Activate custom sound?",
+		["option_sound_check"] = "Should the extra sound be activated?",
+
         ["Facs124_menu"] = "Facs 124",
 		["Eads-y_dr_name"] = "Eads-y DR (1978)",
 		["Eads-y_dr_desc"] = "The first wagons were built in France on behalf of the Deutsche Reichsbahn from 1978. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
@@ -13,13 +19,28 @@ return {
         ["Facs124_db_desc"] = "After the reunification, the Facs 124 also came to the Deutsche Bundesbahn.",
         ["Facs124_dbag_name"] = "Facs 124 DB AG (1994)",
         ["Facs124_dbag_desc"] = "After the founding of DB AG, the Facs 124 continued to be used with the new logo.",
-        ["Facs124_vr_name"] = "Facs 124 verkehrsrot DB AG (2007)",
+        ["Facs124_vr_name"] = "Facs 124 traffic red DB AG (2007)",
         ["Facs124_vr_desc"] = "Later on, the cars were also repainted in traffic red. So far, some of them are still in use.",
+		
+		--- Fake ---
+		
+		["Facs124_menu_fake"] = "Facs 124 Fake",
+		["Eads-y_dr_name_fake"] = "Eads-y DR (1978) Fake",
+		["Facs124_dr_name_fake"] = "Facs DR (1980) Fake",
+		["Facs124_db_name_fake"] = "Facs 124 DB (1991) Fake",
+		["Facs124_dbag_name_fake"] = "Facs 124 DB AG (1994) Fake",
+		["Facs124_vr_name_fake"] = "Facs 124 traffic red DB AG (2007) Fake",		
 	},
 	de = {
 		["mod_name"] = "Facs 124 Drehgestell-Schüttgutwagen",
 		["mod_desc"] = "Die ursprünglich als Eads-y bezeichneten, von der Deutschen Reichsbahn ab 1978 beschafften Wagen sind zur Beförderung von Nässe unempfindlichem Schüttgut vorgesehen. Der Laderaum hat die Form mehrerer nebeneinander stehender Trichter. Für den Entladevorgang wird die Schwerkraft des Ladegutes genutzt. Die Auslauföffnungen mit Rundschiebern ermöglichen die restlose Entladung des Wagens nach der einen oder der anderen Seite. Die schwenkbaren Auslaufrutschen gestatten eine Bunkerentladung.",
 
+        ["Fake_facs124_wagen"] = "Facs124 Fake-Versionen aktivieren?",
+        ["option_fake_facs124_desc"] = "Sollen die Fake-Versionen der Facs 124 verfügbar sein im Spiel?",
+
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["option_sound_check"] = "Soll der extra Sound aktiviert sein?",
+		
         ["Facs124_menu"] = "Facs 124",
 		["Eads-y_dr_name"] = "Eads-y DR (1978)",
 		["Eads-y_dr_desc"] = "Die ersten Wagen wurden im Auftrag der Deutschen Reichsbahn ab 1978 in Frankreich gebaut. Die Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
@@ -31,6 +52,17 @@ return {
         ["Facs124_dbag_desc"] = "Nach Gründung der DB AG wurden die Facs 124 weiterverwendet mit dem neuen Logo.",
         ["Facs124_vr_name"] = "Facs 124 verkehrsrot DB AG (2007)",
         ["Facs124_vr_desc"] = "Im späteren Verlauf wurden die Wagen auch mal in verkehrsrot umlackiert. Bisher sind immer noch einige im Einsatz.",
+		
+		--- Fake ---
+		
+		["Facs124_menu_fake"] = "Facs 124 Fake",
+		["Eads-y_dr_name_fake"] = "Eads-y DR (1978) Fake",
+		["Facs124_dr_name_fake"] = "Facs DR (1980) Fake",
+		["Facs124_db_name_fake"] = "Facs 124 DB (1991) Fake",
+		["Facs124_dbag_name_fake"] = "Facs 124 DB AG (1994) Fake",
+		["Facs124_vr_name_fake"] = "Facs 124 verkehrsrot DB AG (2007) Fake",		
 	},
 }
 end
+
+		
